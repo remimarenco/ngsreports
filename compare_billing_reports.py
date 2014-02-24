221b58e2e3d2fbe63afa2709323345f512dc264d
pajanne <pajanne@gmail.com>
2014-02-24 17:15:49 +0000

    add billing month into comparison

--- compare_billing_reports.py
+++ compare_billing_reports.py
@@ -30,12 +30,12 @@ def main():
             print key
             
     print "--------------------------------------------------------------------------------"
-    print "-- for key with value ['SLXID;run_type;billing_status;flowcellid;lane]" 
+    print "-- for key with value ['SLXID;run_type;billing_status;billing_month;flowcellid;lane]" 
     print "-- in LAST %s DO NOT MATCH in THIS %s" % (options.report1, options.report2)
     print "--------------------------------------------------------------------------------"
     for key, value in data1.iteritems():
         if key in data2.keys():
-            if not value == data2[key]:
+            if len(set(value).intersection(data2[key])) == 0:
                 print '---'
                 print 'LAST: %s' % value
                 print 'THIS: %s' % data2[key]
@@ -62,16 +62,23 @@ def main():
             i += 1
             print i, item
 
-def create_report_from_file(file_report):
+def create_report_from_file(file_report, with_extra=False):
     # file format
-    # "researcher";"lab";"institute";"slxid"***;"runtype"***;"billable"***;"billingmonth";"flowcellid"***;"lane"***;
+    # 0:"researcher";1:"lab";2:"institute";3:"slxid"***;4:"runtype"***;5:"billable"***;6:"billingmonth";7:"flowcellid"***;8:"lane"***;
+    
+    # 0researcher;1lab;2institute;3slxid;4runtype;5billable;6billingmonth;7flowcellid;8lane;9flowcellbillingcomments;10billingcomments;11runfolder;12instrument;13submissiondate;14completiondate;
     data = defaultdict(list)
     with open (file_report, "U") as f:
         for line in f.readlines():
             content = line.strip().replace('"','').split(';')
             if not content[7] in 'flowcellid':
                 key = "%s_%s" % (content[7], content[8])
-                data[key].append(';'.join(content[3:6] + content[7:9]))
+                data[key].append(';'.join(content[3:7] + content[7:9]))
+                if with_extra:
+                    if content[13] == '':
+                        print 'NODATE: %s:%s' % (key, ';'.join(content[3:7] + content[7:9]))
+                    else:
+                        print '  date: %s:%s' % (key, ';'.join(content[3:7] + content[7:9]))
     return data
         
 if __name__ == '__main__':
