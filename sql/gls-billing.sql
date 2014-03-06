d4481663e686eb743cff390bb787fd1ee9a2d350
pajanne <pajanne@gmail.com>
2014-03-06 10:31:15 +0000

    remove sub-query and process join, simplify query by using WITH; change billing month to return billing process daterun instead of sequencing process daterun

--- sql/gls-billing.sql
+++ sql/gls-billing.sql
@@ -1,12 +1,30 @@
 -- --------------------------------------------------------------------------------
--- BILLING QUERY - latest version 31-Jan-2014
+-- BILLING QUERY - latest version 6-Mar-2014
 -- against genologics database v2.5.2.367 http://genologics.com/
+-- --------------------------------------------------------------------------------
+-- changelog:
+-- 05/03/14: remove sub-query and process join, simplify query by using WITH 
+--           change billing month to return billing process daterun instead of 
+--           sequencing process daterun
+-- 26/02/14: add new sample UDF Billing Information 
 -- 31/01/14: add two new UDFs % lost reads and yield
 -- 21/01/14: add avg library length as it can be different over samples in pool
 --           add fields Library Type; Index Type & Average Library Length
 -- 02/07/13: first versions
 -- --------------------------------------------------------------------------------
 
+WITH billing as (
+	SELECT artifact.luid as artifactluid, process.daterun as daterun, pudf1.udfvalue as fcbillingcomments
+	FROM processiotracker, process
+	LEFT OUTER JOIN process_udf_view as pudf1 on (pudf1.processid=process.processid AND pudf1.udfname = 'Flowcell Billing Comments'),
+	processtype, artifact
+	WHERE processiotracker.processid=process.processid 
+	AND process.typeid=processtype.typeid 
+	AND processtype.displayname='Billing' 
+	AND processiotracker.inputartifactid=artifact.artifactid 
+	AND process.daterun >= '2013-04-01' AND process.daterun <= 'BILLING-DATE'
+)
+-- SELECT -------------------------------------------------------------------------
 SELECT distinct
 researcher.firstname || ' ' || researcher.lastname as researcher, 
 lab.name as lab, 
@@ -14,10 +32,10 @@ address.institution as institute,
 sudf1.udfvalue as slxid, 
 itype.name || '_' || case when itype.name = 'Miseq' then split_part(pudf6.udfvalue, '-', 2) else case when pudf2.udfvalue is null then 'SE' else 'PE' end || pudf1.udfvalue end as runtype,
 audf1.udfvalue as billable, 
-to_char(process.daterun, 'YYYY-MM') as billingmonth, 
+to_char(billing.daterun, 'YYYY-MM') as billingmonth, 
 pudf4.udfvalue as flowcellid, 
 'Lane' || containerplacement.wellyposition + 1 as lane, 
-bpudf1.udfvalue as flowcellbillingcomments,
+billing.fcbillingcomments as flowcellbillingcomments,
 audf2.udfvalue as billingcomments,
 pudf5.udfvalue as runfolder, 
 instrument.name as instrument,
@@ -41,8 +59,9 @@ audf11.udfvalue as percent_prephasing_r1,
 audf12.udfvalue as percent_aligned_r1,
 audf13.udfvalue as percent_error_rate_r1,
 ludf1.udfvalue as external
-
-FROM process 
+-- FROM ---------------------------------------------------------------------------
+FROM billing, 
+process 
 LEFT OUTER JOIN process_udf_view as pudf1 on (pudf1.processid=process.processid AND pudf1.udfname like 'Read 1 Cycle%') 
 LEFT OUTER JOIN process_udf_view as pudf2 on (pudf2.processid=process.processid AND pudf2.udfname like 'Read 2 Cycle%') 
 LEFT OUTER JOIN process_udf_view as pudf3 on (pudf3.processid=process.processid AND pudf3.udfname = 'Finish Date')
@@ -81,14 +100,10 @@ LEFT OUTER JOIN sample_udf_view as sudf4 on (sudf4.sampleid=sample.sampleid AND 
 LEFT OUTER JOIN sample_udf_view as sudf5 on (sudf5.sampleid=sample.sampleid AND sudf5.udfname = 'Billing Information'),
 project, 
 researcher, 
-lab LEFT OUTER JOIN entity_udf_view as ludf1 on (lab.labid=ludf1.attachtoid AND ludf1.udfname='External')
-LEFT OUTER JOIN address on (lab.billingaddressid=address.addressid), 
--- billing process
-process as bprocess LEFT OUTER JOIN process_udf_view as bpudf1 on (bpudf1.processid=bprocess.processid AND bpudf1.udfname = 'Flowcell Billing Comments') ,
-processtype as bprocesstype,
-processiotracker as bprocessiotracker,
-artifact as bartifact
-
+lab 
+LEFT OUTER JOIN entity_udf_view as ludf1 on (lab.labid=ludf1.attachtoid AND ludf1.udfname='External')
+LEFT OUTER JOIN address on (lab.billingaddressid=address.addressid)
+-- WHERE --------------------------------------------------------------------------
 WHERE process.typeid = processtype.typeid
 AND processtype.displayname like '%Run%'
 AND process.installationid=installation.id
@@ -103,22 +118,9 @@ AND artifact_sample_map.processid=sample.processid
 AND sample.projectid=project.projectid
 AND project.researcherid=researcher.researcherid
 AND researcher.labid=lab.labid
---AND ludf1.udfvalue='True'
-AND artifact.luid IN ( 
-	SELECT sub_artifact.luid
-	FROM processiotracker as sub_processiotracker, process as sub_process, processtype as sub_processtype, artifact as sub_artifact
-	WHERE sub_processiotracker.processid=sub_process.processid 
-	AND sub_process.typeid=sub_processtype.typeid 
-	AND sub_processtype.displayname='Billing' 
-	AND sub_processiotracker.inputartifactid=sub_artifact.artifactid 
-	AND sub_process.daterun >= '2013-04-01' AND sub_process.daterun <= 'BILLING-DATE'
-) 
--- billing process
-AND artifact.luid=bartifact.luid
-AND bprocess.typeid = bprocesstype.typeid
-AND bprocessiotracker.processid=bprocess.processid
-AND bprocessiotracker.inputartifactid=bartifact.artifactid
-AND bprocesstype.displayname='Billing'
-GROUP BY researcher, lab, institute, slxid, runtype, billable, billingmonth, flowcellid, lane, flowcellbillingcomments, billingcomments, runfolder, instrument, submissiondate, completiondate, 
- billing_code, library_type, index_type, yield_pf_r1, avg_q_score_r1, percent_bases_q30_r1, yield, percent_lost_reads, cluster_density_r1, percent_pf_r1, 
- intensity_cycle_1_r1, percent_intensity_cycle_20_r1, percent_phasing_r1, percent_prephasing_r1, percent_aligned_r1, percent_error_rate_r1, external
+AND artifact.luid=billing.artifactluid
+-- GROUP BY -----------------------------------------------------------------------
+GROUP BY researcher, lab, institute, slxid, runtype, billable, billingmonth, flowcellid, lane, flowcellbillingcomments, billingcomments, runfolder, 
+instrument, submissiondate, completiondate, billing_code, library_type, index_type, yield_pf_r1, avg_q_score_r1, percent_bases_q30_r1, yield, 
+percent_lost_reads, cluster_density_r1, percent_pf_r1, intensity_cycle_1_r1, percent_intensity_cycle_20_r1, percent_phasing_r1, percent_prephasing_r1, 
+percent_aligned_r1, percent_error_rate_r1, external
