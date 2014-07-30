f646aa1392afd8a64a860dc118020b465d861d77
pajanne <pajanne@gmail.com>
2014-07-30 19:28:01 +0100

    add missing file for new billing script

---
+++ billing.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+query() {
+    TEMPLATE=$1
+    QUERY=$2
+    CSVOUT=$3
+    
+    # copy query
+    cp $TEMPLATE $QUERY
+
+    # run query
+    export PGPASSWORD=readonly
+    psql -h lims -U readonly -d "clarityDB"  -c "COPY ( `cat $QUERY` ) TO STDOUT WITH DELIMITER AS E'\t' CSV HEADER " > $CSVOUT || echo "ERROR: Unable to run $QUERY" > $CSVOUT
+    # test - quick/simple query: psql -h lims -U readonly -d "clarityDB"  -c "COPY ( select * from researcher ) TO STDOUT WITH DELIMITER AS E'\t' CSV HEADER " > $CSVOUT
+}
+
+run() {
+    BILLING_DATE=$1
+    BILLING_PREVDATE=$2
+    GROUPREPORT_DATE=$3
+    
+    BASEDIR=$(dirname $0)
+    SQLDIR=$BASEDIR/sql/
+    OUTPUTDIR=$BASEDIR/billing/
+
+    BILLING_TEMPLATE=$SQLDIR/gls-billing.sql
+    BILLING_QUERY=$OUTPUTDIR/$BILLING_DATE-gls-billing.sql
+    BILLING_CSVOUT=$OUTPUTDIR/$BILLING_DATE-billing.csv
+    BILLING_LASTCVSOUT=$OUTPUTDIR/$BILLING_PREVDATE-billing.csv
+    COMPAREOUT=$OUTPUTDIR/$BILLING_DATE-compare-reports.out
+
+    ACCOUNT_TEMPLATE=$SQLDIR/gls-account.sql
+    ACCOUNT_QUERY=$OUTPUTDIR/$BILLING_DATE-gls-account.sql
+    ACCOUNT_CSV=$OUTPUTDIR/$BILLING_DATE-account.csv
+    
+    PRICES_CSV=$BASEDIR/pricing/PricingSummaryTable.txt
+
+    QC_TEMPLATE=$SQLDIR/gls-qc.sql
+    QC_QUERY=$OUTPUTDIR/$BILLING_DATE-gls-qc.sql
+    QC_CSVOUT=$OUTPUTDIR/$BILLING_DATE-qc.csv
+
+    # billing query
+    query $BILLING_TEMPLATE $BILLING_QUERY $BILLING_CSVOUT 
+
+    # account query
+    query $ACCOUNT_TEMPLATE $ACCOUNT_QUERY $ACCOUNT_CSV
+
+    # write reports
+    python $BASEDIR/reports.py --report=$BILLING_CSVOUT --accounts=$ACCOUNT_CSV --prices=$PRICES_CSV --date=$GROUPREPORT_DATE --outputdir=$BASEDIR
+
+    # write comparison & send email notification
+    python $BASEDIR/autobilling.py --last-month-report=$BILLING_LASTCVSOUT --this-month-report=$BILLING_CSVOUT --output=$COMPAREOUT --email
+
+    # qc query
+    query $QC_TEMPLATE $QC_QUERY $QC_CSVOUT
+}
+
